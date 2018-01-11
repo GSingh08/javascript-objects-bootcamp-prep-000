55528978ccf243479c9ba9d88335d6f4296d28a4
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-11 22:30:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist( playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-delete playlist.taylor swift;
+delete playlist.'taylor swift';
 return playlist;
 }
