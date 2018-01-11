a72d40d75476e738233836dd39dffbc938a2982a
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-11 22:18:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'taylor swift':"love story", 'john cena': "you cant see me"};
 
 function updatePlayList( playlist, artistName, songTitle){
-  playlist.lilwayne = "fireman";
-  return playlist
+  playlist['LilWayne'] = "fireman";
+  
   
 }
